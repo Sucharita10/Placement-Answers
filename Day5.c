5913c6829a0953b7b621bd6185ede4ae2af93fb0
Sucharita Pradhan <sucharitapradhan101@gmail.com>
2023-09-12 21:01:51 +0530

    Add files via upload

---
+++ Day5.c
@@ -0,0 +1,28 @@
+
+1.(c) 52500
+2.(d) Statement 2 is the cause and statement 1 is the effect
+3. (b) Financial success without emotional balance can cause havoc in a person's life.
+4.
+
+#include<stdio.h>
+int main(){
+    int a, b;
+    int t1=0, t2=1;
+    int res=t1+t2;
+    printf("Enter the number of terms:");
+    scanf("%d",&b);
+    printf("Fibonacci series: %d ",t2);
+    for(a=2; a<=b; ++a){
+    printf("%d " , res);
+    t1=t2;
+    t2=res;
+    res=t1+t2;
+    }
+    return 0;
+    }
+5. (2)
+6. (d) eu
+    
+
+
+
