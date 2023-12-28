25271294919005ca10760c4a693fb3587b088f01
Sucharita Pradhan <sucharitapradhan101@gmail.com>
2023-12-28 13:19:10 +0530

    Add files via upload

---
+++ Day 12.c
@@ -0,0 +1,13 @@
+1.(a)240
+2.(b)5/126
+3.(b)CBAD
+4.(b)%%
+5. Algorithm:-
+begin BubbleSort(arr)  
+   for all array elements  
+      if arr[i] > arr[i+1]  
+         swap(arr[i], arr[i+1])  
+      end if  
+   end for     
+   return arr     
+end BubbleSort
